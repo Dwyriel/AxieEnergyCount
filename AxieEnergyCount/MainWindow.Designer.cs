2713721ed218bc72fb0b29a9d5926cada2661e7d
Dwyriel <dwyriel333@hotmail.com>
2021-08-26 03:11:54 -0300

    Initial commit

---
+++ AxieEnergyCount/MainWindow.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace AxieEnergyCount
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.OptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BackgroundSubmenuBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitSubmenuBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.BackgroundImage4 = new System.Windows.Forms.PictureBox();
+            this.BackgroundImage3 = new System.Windows.Forms.PictureBox();
+            this.BackgroundImage2 = new System.Windows.Forms.PictureBox();
+            this.BackgroundImage1 = new System.Windows.Forms.PictureBox();
+            this.PicBoxBG1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxBG1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OptionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(400, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // OptionsToolStripMenuItem
+            // 
+            this.OptionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BackgroundSubmenuBtn,
+            this.exitSubmenuBtn});
+            this.OptionsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem";
+            this.OptionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.OptionsToolStripMenuItem.Text = "Options";
+            // 
+            // BackgroundSubmenuBtn
+            // 
+            this.BackgroundSubmenuBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundSubmenuBtn.Name = "BackgroundSubmenuBtn";
+            this.BackgroundSubmenuBtn.Size = new System.Drawing.Size(180, 22);
+            this.BackgroundSubmenuBtn.Text = "Background";
+            this.BackgroundSubmenuBtn.Click += new System.EventHandler(this.BackgroundSubmenuBtn_Click);
+            // 
+            // exitSubmenuBtn
+            // 
+            this.exitSubmenuBtn.Name = "exitSubmenuBtn";
+            this.exitSubmenuBtn.Size = new System.Drawing.Size(180, 22);
+            this.exitSubmenuBtn.Text = "Exit";
+            this.exitSubmenuBtn.Click += new System.EventHandler(this.exitSubmenuBtn_Click);
+            // 
+            // BackgroundImage4
+            // 
+            this.BackgroundImage4.Image = global::AxieEnergyCount.Properties.Resources.tumblr_pcltzsiH4h1wceprro6_400;
+            this.BackgroundImage4.Location = new System.Drawing.Point(162, 187);
+            this.BackgroundImage4.Name = "BackgroundImage4";
+            this.BackgroundImage4.Size = new System.Drawing.Size(109, 69);
+            this.BackgroundImage4.TabIndex = 5;
+            this.BackgroundImage4.TabStop = false;
+            this.BackgroundImage4.Visible = false;
+            // 
+            // BackgroundImage3
+            // 
+            this.BackgroundImage3.Image = global::AxieEnergyCount.Properties.Resources.cat_angela;
+            this.BackgroundImage3.Location = new System.Drawing.Point(147, 172);
+            this.BackgroundImage3.Name = "BackgroundImage3";
+            this.BackgroundImage3.Size = new System.Drawing.Size(109, 69);
+            this.BackgroundImage3.TabIndex = 4;
+            this.BackgroundImage3.TabStop = false;
+            this.BackgroundImage3.Visible = false;
+            // 
+            // BackgroundImage2
+            // 
+            this.BackgroundImage2.Image = global::AxieEnergyCount.Properties.Resources.seraph_of_the_end_stare;
+            this.BackgroundImage2.Location = new System.Drawing.Point(131, 155);
+            this.BackgroundImage2.Name = "BackgroundImage2";
+            this.BackgroundImage2.Size = new System.Drawing.Size(109, 69);
+            this.BackgroundImage2.TabIndex = 3;
+            this.BackgroundImage2.TabStop = false;
+            this.BackgroundImage2.Visible = false;
+            // 
+            // BackgroundImage1
+            // 
+            this.BackgroundImage1.Image = global::AxieEnergyCount.Properties.Resources.nekoha_anime;
+            this.BackgroundImage1.Location = new System.Drawing.Point(117, 142);
+            this.BackgroundImage1.Name = "BackgroundImage1";
+            this.BackgroundImage1.Size = new System.Drawing.Size(109, 69);
+            this.BackgroundImage1.TabIndex = 2;
+            this.BackgroundImage1.TabStop = false;
+            this.BackgroundImage1.Visible = false;
+            // 
+            // PicBoxBG1
+            // 
+            this.PicBoxBG1.Location = new System.Drawing.Point(0, 19);
+            this.PicBoxBG1.Name = "PicBoxBG1";
+            this.PicBoxBG1.Size = new System.Drawing.Size(400, 400);
+            this.PicBoxBG1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicBoxBG1.TabIndex = 0;
+            this.PicBoxBG1.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 419);
+            this.Controls.Add(this.BackgroundImage4);
+            this.Controls.Add(this.BackgroundImage3);
+            this.Controls.Add(this.BackgroundImage2);
+            this.Controls.Add(this.BackgroundImage1);
+            this.Controls.Add(this.PicBoxBG1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "Axie Energy Count";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxBG1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicBoxBG1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem OptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem BackgroundSubmenuBtn;
+        private System.Windows.Forms.ToolStripMenuItem exitSubmenuBtn;
+        private System.Windows.Forms.PictureBox BackgroundImage1;
+        private System.Windows.Forms.PictureBox BackgroundImage2;
+        private System.Windows.Forms.PictureBox BackgroundImage3;
+        private System.Windows.Forms.PictureBox BackgroundImage4;
+    }
+}
+
---
+++ AxieEnergyCount/MainWindow.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AxieEnergyCount
+{
+    public partial class MainWindow : Form
+    {
+        List<Image> BackgroundImages = new List<Image>();
+        int enemyEnergy = 3, wins = 0, counter = 0;
+
+        //Set clock timer to render gif at 100% speed:
+        private const int timerAccuracy = 10;
+        [System.Runtime.InteropServices.DllImport("winmm.dll")]
+        private static extern int timeBeginPeriod(int msec);
+        [System.Runtime.InteropServices.DllImport("winmm.dll")]
+        public static extern int timeEndPeriod(int msec);
+
+        public MainWindow()
+        {
+            SetStartAttributes();
+            InitializeComponent();
+            BackgroundSetup(); AddOneWin();
+        }
+
+        void SetStartAttributes()
+        {
+            timeBeginPeriod(timerAccuracy);
+            StartPosition = FormStartPosition.CenterScreen;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+        }
+
+        void BackgroundSetup()
+        {
+            AddImagesToList();
+            PicBoxBG1.Image = BackgroundImages[counter];
+        }
+
+        void AddImagesToList()
+        {
+            BackgroundImages.Add(BackgroundImage1.Image);
+            BackgroundImages.Add(BackgroundImage2.Image);
+            BackgroundImages.Add(BackgroundImage3.Image);
+            BackgroundImages.Add(BackgroundImage4.Image);
+        }
+
+        private void AddOne()
+        {
+            enemyEnergy = Clamp(++enemyEnergy, 0, 10);
+        }
+
+        private void MinusOne()
+        {
+            enemyEnergy = Clamp(--enemyEnergy, 0, 10);
+        }
+
+        private void NextTurn()
+        {
+            enemyEnergy = Clamp(enemyEnergy + 2, 0, 10);
+        }
+
+        private void NewGame()
+        {
+            enemyEnergy = 3;
+        }
+
+        private void AddOneWin()
+        {
+            wins = Clamp(++wins, 0, int.MaxValue);
+        }
+
+        private void MinusOneWin()
+        {
+            wins = Clamp(--wins, 0, int.MaxValue);
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timeEndPeriod(timerAccuracy);
+        }
+
+        private void BackgroundSubmenuBtn_Click(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter >= BackgroundImages.Count)
+                counter = 0;
+            PicBoxBG1.Image = BackgroundImages[counter];
+        }
+
+        private void exitSubmenuBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private int Clamp(int number, int min, int max)
+        {
+            return (number < min) ? min : (number > max) ? max : number;
+        }
+    }
+
+    public class CustomLabel : Label
+    {
+        public CustomLabel()
+        {
+            OutlineForeColor = Color.Green;
+            OutlineWidth = 2;
+        }
+        public Color OutlineForeColor { get; set; }
+        public float OutlineWidth { get; set; }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(BackColor), ClientRectangle);
+            using (GraphicsPath gp = new GraphicsPath())
+            using (Pen outline = new Pen(OutlineForeColor, OutlineWidth)
+            { LineJoin = LineJoin.Round })
+            using (StringFormat sf = new StringFormat())
+            using (Brush foreBrush = new SolidBrush(ForeColor))
+            {
+                gp.AddString(Text, Font.FontFamily, (int)Font.Style,
+                    Font.Size, ClientRectangle, sf);
+                e.Graphics.ScaleTransform(1.3f, 1.35f);
+                e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+                e.Graphics.DrawPath(outline, gp);
+                e.Graphics.FillPath(foreBrush, gp);
+            }
+        }
+    }
+}
